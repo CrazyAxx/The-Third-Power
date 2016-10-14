f5618677f122c586e49a2a6045d32b2bc5fa6928
CrazyAxx <244503@bwls.net>
2016-10-14 12:18:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-
+  createCanvas( windowWidth , windowHeight , WEBGL );
 }
 
 function draw() {
-
+  background(200);
+  rotateX( framecount * 0.01 );
+  rotateY( framecount * 0.01 );
+  box( 200 , 200 , 200 );
 }
