a90266c6b445ab9bd51bce08975a75e644c4f118
Collin Bates <244503@bwls.net>
2016-10-19 11:56:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,17 +2,10 @@ function setup() {
   createCanvas( 1280 , 800 , WEBGL )
 }
 
-  var x=200
-  var y=200
-  var z=200
-
 function draw() {
   background( 1380 , 900 );
   rotateX( frameCount * 0.01 );
   rotateY( frameCount * 0.01 );
   translate( mouseX , mouseY , 0 );
-  box( x , y , z );
-  x=x+.1
-  y=y+.1
-  z=z+.1
+  box( 200 , 200 , 200 );
 }
